8e7de994e2d3b4cd788bf6a21fff55b83bdbd6bb
Camila Díaz <cdiazmorel@gmail.com>
2022-10-25 00:13:48 -0300

    uego Base
    Juego Base, con diversas cosas modificadas

---
+++ core/src/com/mygdx/game/GameOverScreen.java
@@ -0,0 +1,84 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameOverScreen implements Screen {
+	private final GameLluviaMenu game;
+	private SpriteBatch batch;	   
+	private BitmapFont font;
+	private OrthographicCamera camera;
+
+	public GameOverScreen(final GameLluviaMenu game) {
+		this.game = game;
+        this.batch = game.getBatch();
+        this.font = game.getFont();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+	}
+
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+		font.draw(batch, "GAME OVER ", 100, 100);
+		font.draw(batch, "Presione Enter para jugar de nuevo.", 100, 200);
+		font.draw(batch, "Presione Escape para salir para jugar de nuevo.", 100, 300);
+		batch.end();
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) 
+		{
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
+		else if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) 
+		{
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

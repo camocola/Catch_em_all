81ea3d18a48f6cc988bffabfd14f7f39d506517d
Camila Díaz <cdiazmorel@gmail.com>
2022-10-25 21:57:36 -0300

    Interfaz para objetos colisionables

--- core/src/com/mygdx/game/Colisionable.java
+++ core/src/com/mygdx/game/Colisionable.java
@@ -1,9 +1,9 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.math.Rectangle;
-
 public interface Colisionable
 {
-	boolean onColision();
-	boolean verColision (Rectangle obj, Arquero goalkeeper, int i);
+	//boolean onColision();
+	
+	//Verifica si el arquero colisionó con algún objeto 
+	boolean verColision (Arquero gk);
 }

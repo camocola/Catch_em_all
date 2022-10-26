d74e6bc6280a3229ea067443de198a54b7e1c733
Camila Díaz <cdiazmorel@gmail.com>
2022-10-25 22:00:27 -0300

    Se crea clase abstracta para diferentes proyectiles

---
+++ core/src/com/mygdx/game/Proyectiles.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Proyectiles extends Objeto
+{
+	private int velX;
+	private int velY;
+	
+	public Proyectiles(Sound s, Texture i) 
+	{
+		super(s, i);
+		// TODO Auto-generated constructor stub
+	}
+	
+}

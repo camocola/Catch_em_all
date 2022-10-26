1ec9d2deba92a251e1e8390f4a8e083722d8b7d0
Camila Díaz <cdiazmorel@gmail.com>
2022-10-25 21:58:22 -0300

    Clase abstracta para diferentes balones

---
+++ core/src/com/mygdx/game/Balon.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class Balon extends Objeto
+{
+
+	private int velY;
+	
+	public Balon(Sound s, Texture i, int vY) 
+	{
+		super(s, i);
+		velY = vY;
+	}
+	
+	public void move()
+	{
+		Rectangle r = getR();
+		r.y -= velY * Gdx.graphics.getDeltaTime();
+	}
+	
+}

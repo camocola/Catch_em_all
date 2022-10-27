e29bca54c8be2f8abc50bad762efc9a5b0f76c93
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-10-27 17:21:00 -0300

    Se elimina comentario inecesario

--- core/src/com/mygdx/game/Colisionable.java
+++ core/src/com/mygdx/game/Colisionable.java
@@ -2,7 +2,6 @@ package com.mygdx.game;
 
 public interface Colisionable extends Dibujable
 {
-	//boolean onColision();
 	//Verifica si el arquero colisionó con algún objeto 
 	boolean onColision (Arquero gk);
 }

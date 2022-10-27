3787021bde5017774a16071a6e05b1a081425208
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-10-27 16:39:21 -0300

    Colisionable hereda de dibujable

--- core/src/com/mygdx/game/Colisionable.java
+++ core/src/com/mygdx/game/Colisionable.java
@@ -1,6 +1,6 @@
 package com.mygdx.game;
 
-public interface Colisionable
+public interface Colisionable extends Dibujable
 {
 	//boolean onColision();
 	//Verifica si el arquero colisionó con algún objeto 

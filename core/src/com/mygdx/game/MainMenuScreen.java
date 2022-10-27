851db0062c65001af56e517bf873e0821f044db0
Camila Díaz <cdiazmorel@gmail.com>
2022-10-27 19:51:32 -0300

    se cambia mensaje

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -33,7 +33,7 @@ public class MainMenuScreen implements Screen {
 
 		batch.begin();
 		font.getData().setScale(2, 2);
-		font.draw(batch, "Bienvenido a Recolecta Gotas!!! ", 100, camera.viewportHeight/2+50);
+		font.draw(batch, "Bienvenido a Catch 'em all!!! ", 100, camera.viewportHeight/2+50);
 		font.draw(batch, "Toca en cualquier lugar para comenzar!", 100, camera.viewportHeight/2-50);
 
 		batch.end();
